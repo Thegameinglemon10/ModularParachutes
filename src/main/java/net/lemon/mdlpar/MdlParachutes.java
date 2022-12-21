b8224cb6cdd309059dd2ad4c838b23e08cf89cdb
Thegameinglemon10 <49798130+Thegameinglemon10@users.noreply.github.com>
2022-12-21 11:21:37 -0600

    Initial commit

---
+++ src/main/java/net/lemon/mdlpar/MdlParachutes.java
@@ -0,0 +1,15 @@
+package net.lemon.mdlpar;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MdlParachutes implements ModInitializer {
+	public static final String MOD_ID = "mdlpar";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lemon/mdlpar/MdlParachutesClient.java
@@ -0,0 +1,10 @@
+package net.lemon.mdlpar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MdlParachutesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
